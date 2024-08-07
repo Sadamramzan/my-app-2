a6863adc04dbf78c59b6921248077eb9e92c88b5
sadamramzan <sadamramzan83@gmail.com>
2024-08-07 13:32:18 +0500

    second cammand

--- src/app/api/hello/route.tsx
+++ src/app/api/hello/route.tsx
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ message: "hello Next Js" });
+  return NextResponse.json({ message: "hello Next Js 2" });
 }
